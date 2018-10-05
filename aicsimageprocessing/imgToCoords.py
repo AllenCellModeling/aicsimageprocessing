2e1f154627265da13462cb3c6f58beccb701a0ab
Gregory Johnson <gregj@alleninstitute.org>
2018-10-05 11:35:39 -0700

    minor bugfixes"

--- aicsimageprocessing/imgToCoords.py
+++ aicsimageprocessing/imgToCoords.py
@@ -1,24 +1,26 @@
 import numpy as np
 import aicsimageprocessing as proc
 import skfmm
+from scipy.ndimage.morphology import distance_transform_edt as bwdist
+import math
+from skimage.measure import regionprops
 
-def imgtocoords(im_cell, im_nuc, major_angle='cell')
-        """
-        Return unit vector of v
-        :param im_cell: zyx binary image of a cell shape
-        :param im_nuc: zyx binary image of a nuclear shape
-        
-        :return: im_ratio - channels corresponding to ratio image (1 at cell boundary, 0 on nuclear boundary, -1 inside nucleus)
-                 im_th - spherical coordinate system theta (radians)
-                 im_phi - spherical coordinate system phi (radians)
-                 im_r - radial distance from center of nucleus
-                 major_angle - angle of cell or nuclear shape (radians)
-        """
+def imgtocoords(im_cell, im_nuc, major_angle='cell'):
+    """
+    Return unit vector of v
+    :param im_cell: zyx binary image of a cell shape
+    :param im_nuc: zyx binary image of a nuclear shape
+
+    :return: im_ratio - channels corresponding to ratio image (1 at cell boundary, 0 on nuclear boundary, -1 inside nucleus)
+             im_th - spherical coordinate system theta (radians)
+             im_phi - spherical coordinate system phi (radians)
+             im_r - radial distance from center of nucleus
+             major_angle - angle of cell or nuclear shape (radians)
+    """
     
-
     cell_dist_in = skfmm.distance(np.ma.MaskedArray(im_cell, im_nuc)).data
     cell_dist_out = skfmm.distance(np.ma.MaskedArray(im_nuc==0, im_cell==0)).data
-
+    
     nuc_dist = bwdist(im_nuc)
     nuc_ratio = -nuc_dist/np.max(nuc_dist)
     nuc_ratio[np.isnan(nuc_ratio)] = 0
