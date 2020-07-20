97895b04798c31e4e8057b8be0275b778738248d
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-07-20 14:55:00 -0700

    Bump version: 0.7.3 → 0.7.4

--- setup.py
+++ setup.py
@@ -111,6 +111,6 @@ setup(
     url="https://github.com/AllenCellModeling/aicsimageprocessing",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.7.3",
+    version="0.7.4",
     zip_safe=False,
 )
